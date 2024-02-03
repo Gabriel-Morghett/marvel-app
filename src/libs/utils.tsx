9a307a7dafdb54cbc4139ffe523afa5f8eed1059
Gabriel Morghett <gabriel.morghett@gmail.com>
2024-02-03 19:16:29 -0300

    Added toast message to Search hero screen

--- src/libs/utils.tsx
+++ src/libs/utils.tsx
@@ -1,4 +1,6 @@
 import {md5} from 'js-md5';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const API_URL = process.env.REACT_APP_BASE_URL;
 
@@ -15,13 +17,18 @@ const fetchHeroes = async (value: string, publicKey: string, privateKey: string)
     let url = `${baseUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}&nameStartsWith=${value}`
   
     try {
-        console.log("dando fetch na API agora")
         const response = await fetch(url)
         if (!response.ok) {
+            toast.error("Failed search! Change your keys and try again", {
+                position: "bottom-center",
+            });
             throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
         }
         const data = await response.json()
         console.log(data)
+        toast.success("Search was successfull", {
+            position: "bottom-center",
+        });
         return data
     } catch (err) {
         console.error("Erro no fetchHero", err)
